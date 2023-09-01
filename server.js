91e698cee101ab803c76f2770be97cce9fcff0f5
Nathan Greeff <nathangreeff@hotmail.com>
2023-09-01 15:49:37 -0600

    add to server js

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
 
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
